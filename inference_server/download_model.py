a6dfaa4533ed4418a950a9ba0b87e4fbdbabd7ad
Sina Pakseresht <sina.pakseresht@divar.ir>
2023-08-04 20:22:16 +0330

    Don't download model, Specify host in uvicorn

--- inference_server/download_model.py
+++
@@ -1,7 +0,0 @@
-import os
-from transformers import AutoModel, AutoTokenizer
-
-
-if __name__ == '__main__':
-    text_encoder = AutoModel.from_pretrained(os.environ['TEXT_ENCODER_MODEL'])
-    text_tokenizer = AutoTokenizer.from_pretrained(os.environ['TEXT_ENCODER_MODEL'])
