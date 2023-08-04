57367b1919f2b76c89525f78d5262d1c971d64f1
Sina Pakseresht <sina.pakseresht@divar.ir>
2023-08-04 18:59:57 +0330

    Change download way, Fix model name

--- inference_server/download_model.py
+++ inference_server/download_model.py
@@ -1,8 +1,7 @@
-from huggingface_hub import hf_hub_download
-import joblib
 import os
+from transformers import AutoModel, AutoTokenizer
+
 
 if __name__ == '__main__':
-    model = joblib.load(
-        hf_hub_download(repo_id=os.environ['HF_REPO_ID'], filename=os.environ['MODEL_NAME'])
-    )
+    text_encoder = AutoModel.from_pretrained(os.environ['TEXT_ENCODER_MODEL'])
+    text_tokenizer = AutoTokenizer.from_pretrained(os.environ['TEXT_ENCODER_MODEL'])
