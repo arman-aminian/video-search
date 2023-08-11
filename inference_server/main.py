1fff00babe169e3bba7b5189402f2997aa5cf5c9
Sina Pakseresht <sina.pakseresht@divar.ir>
2023-08-11 12:43:41 +0330

    Update inference server to support english language

--- inference_server/main.py
+++ inference_server/main.py
@@ -12,21 +12,29 @@ client = QdrantClient("https://qdrant-mlsd-video-search.darkube.app", port=443)
 
 app = FastAPI()
 
-text_encoder = AutoModel.from_pretrained(os.environ['TEXT_ENCODER_MODEL'])
-text_tokenizer = AutoTokenizer.from_pretrained(os.environ['TEXT_ENCODER_MODEL'])
+text_encoders = {
+    "farsi": AutoModel.from_pretrained(os.environ['TEXT_ENCODER_MODEL_FARSI']),
+    "english": AutoModel.from_pretrained(os.environ['TEXT_ENCODER_MODEL_ENGLISH']),
+}
+text_tokenizers = {
+    "farsi": AutoTokenizer.from_pretrained(os.environ['TEXT_ENCODER_MODEL_FARSI']),
+    "english": AutoTokenizer.from_pretrained(os.environ['TEXT_ENCODER_MODEL_ENGLISH']),
+}
 
 
-@app.get("/{video_name}/")
+@app.get("/{language}/{video_name}/")
 async def query(
+    language: str = Path(..., title="Language", description="Language of the search entry, Can be 'farsi' or 'english'"),
     video_name: str = Path(..., title="Video Name", description="Name of the video or 'ALL' to search in all videos"),
-    search_entry: str = Query(..., title="Search Entry", description="The search entry for text embedding"),
+    search_entry: str = Query(..., title="Search Entry", description="The search entry for searching it in the database"),
 ):
     """
         Query for video frames based on the provided text search entry.
 
         Parameters:
+        - **language** (str): Language of the 'search_entry', Can be 'farsi' or 'english'.
         - **video_name** (str): Name of the video or 'ALL' to search in all videos.
-        - **search_entry** (str): The search entry for text embedding.
+        - **search_entry** (str): The search entry for searching it in the database.
 
         Returns:
         - **List[dict]**: A list of dictionaries containing the search results.
@@ -36,25 +44,25 @@ async def query(
             - **image_base64** (str): The base64-encoded image of the matched frame.
     """
 
-    print(f"query for video {video_name}, search_entry: {search_entry}")
+    print(f"{language} query for video {video_name}, search entry: {search_entry}.")
 
     # text embedding
     with torch.no_grad():
-        tokenized = text_tokenizer(search_entry, return_tensors='pt')
-        text_embedding = text_encoder(**tokenized).pooler_output.squeeze().cpu().tolist()
+        tokenized = text_tokenizers[language](search_entry, return_tensors='pt')
+        text_embedding = text_encoders[language](**tokenized).pooler_output.squeeze().cpu().tolist()
 
     # query vector DB
     if video_name == "ALL":
         results = client.search(
-            collection_name="video_frames",
+            collection_name=f"video_frames_{language}",
             query_vector=text_embedding,
-            limit=10,
+            limit=20,
         )
     else:
         results = client.search(
-            collection_name="video_frames",
+            collection_name=f"video_frames_{language}",
             query_vector=text_embedding,
-            limit=10,
+            limit=20,
             query_filter=Filter(
                 must=[
                     FieldCondition(
